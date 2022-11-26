25e1651a56f8ad2afb295e549fbb7efb7d996264
kibzrael <kibraphael7@gmail.com>
2022-11-26 14:13:45 +0300

    pulsar inc migrate

--- lib/providers/messages_provider.dart
+++ lib/providers/messages_provider.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pulsar/classes/message.dart';
 
 class MessagesProvider extends ChangeNotifier {
@@ -10,14 +8,7 @@ class MessagesProvider extends ChangeNotifier {
     initialize();
   }
 
-  initialize() async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    QuerySnapshot<Map<String, dynamic>> chats = await firestore
-        .collection('Messaging')
-        .where('members.id', arrayContains: 1)
-        .get();
-    Fluttertoast.showToast(msg: chats.docs.toString());
-  }
+  initialize() async {}
 
   clearMessages() {
     selectedMessages.clear();
